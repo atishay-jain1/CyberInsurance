cefc5d2b0ae6c0df3f3c8fe2d495a3f1e9f0272c
atishay-jain1 <atishayjain11.98@gmail.com>
2021-11-09 16:50:48 +0530

    front and back

---
+++ backend/CyberInsurance/src/main/java/com/cyber/insurance/CyberInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.cyber.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberInsuranceApplication.class, args);
+	}
+
+}
---
+++ backend/CyberInsurance/src/test/java/com/cyber/insurance/CyberInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
